e197f9aabc588196b3378a45f039abb464e61ee1
lisernia <lisernia@gmail.com>
2022-01-02 00:03:21 +0000

    primer commit

---
+++ controles/webapp/controller/MasterProductos.controller.js
@@ -0,0 +1,54 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+
+    return Controller.extend("ns.controles.controller.MasterProductos", {
+
+        onInit: function () {
+
+        },
+
+        showSupplierName: function (oEvent) {
+
+            var itemPressed = oEvent.getSource();
+            var oContext = itemPressed.getBindingContext("products_json");
+            var objectContext = oContext.getObject();
+
+            sap.m.MessageToast.show(objectContext.SupplierName);
+
+        },
+
+
+
+
+
+        onFilter: function () {
+            var oJSON = this.getView().getModel("products_json").getData();
+            var filters = [];
+            if (oJSON.CurrencyCode !== "") {
+                filters.push(new Filter("CurrencyCode", FilterOperator.EQ, oJSON.CurrencyCode))
+            }
+
+            if (oJSON.CurrencyCode !== "" && oJSON.CurrencyCode !== "undefined") {
+                filters.push(new Filter("CurrencyCode", FilterOperator.EQ, oJSON.CurrencyCode))
+            }
+
+            if (oJSON.Description !== "" && oJSON.Description !== "undefined") {
+
+                filters.push(new Filter("Description", FilterOperator.Contains, oJSON.Description.trim()))
+            }
+
+            var oList = this.getView().byId("idProductoTable");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+        },
+
+
+        onClearFilter: function () {
+            var oModel = this.getView().getModel("products_json").getData();
+            oModel.setProperty("/CurrencyCode", "");
+            oModel.setProperty("/Description", "");
+        },
+
+    });
+}); 
---
+++ controles/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/controles/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
